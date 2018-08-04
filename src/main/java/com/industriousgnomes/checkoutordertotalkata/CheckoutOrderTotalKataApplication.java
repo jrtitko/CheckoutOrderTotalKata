e43da70fb4602b5b1af476018e4d5aa6d98e1174
J.R. Titko <j.r.titko.professional@gmail.com>
2018-08-04 08:59:15 -0500

    Initial Commit

---
+++ src/main/java/com/industriousgnomes/checkoutordertotalkata/CheckoutOrderTotalKataApplication.java
@@ -0,0 +1,12 @@
+package com.industriousgnomes.checkoutordertotalkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutOrderTotalKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutOrderTotalKataApplication.class, args);
+    }
+}
