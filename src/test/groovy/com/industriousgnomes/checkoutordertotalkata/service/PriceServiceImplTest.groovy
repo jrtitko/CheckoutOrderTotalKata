631f919a0bfa80cff1e8727f13159d939476302d
J.R. Titko <j.r.titko.professional@gmail.com>
2018-11-01 14:16:49 -0500

    RED - Get the price of an item

---
+++ src/test/groovy/com/industriousgnomes/checkoutordertotalkata/service/PriceServiceImplTest.groovy
@@ -0,0 +1,25 @@
+package com.industriousgnomes.checkoutordertotalkata.service
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class PriceServiceImplTest extends Specification {
+
+    @Subject
+    PriceService priceService
+
+    void setup() {
+        priceService = new PriceServiceImpl()
+    }
+
+    def "Should get the price of an item"() {
+        given:
+            def item = "bread"
+
+        when:
+            def price = priceService.getPrice(item)
+
+        then:
+            price == 1.00
+    }
+}
