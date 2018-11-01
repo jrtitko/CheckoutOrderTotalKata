ca63197c5c08db5e5ebd022d10e0b1af58ac68a3
J.R. Titko <j.r.titko.professional@gmail.com>
2018-11-01 14:00:04 -0500

    GREEN - Retrieve the value of a scanned item

--- src/main/java/com/industriousgnomes/checkoutordertotalkata/api/CheckoutOrder.java
+++ src/main/java/com/industriousgnomes/checkoutordertotalkata/api/CheckoutOrder.java
@@ -2,6 +2,13 @@ package com.industriousgnomes.checkoutordertotalkata.api;
 
 public class CheckoutOrder {
 
+    private double total = 0;
+
     public void scanItem(String item) {
+        total = 1.00;
+    }
+
+    public double getTotal() {
+        return total;
     }
 }
